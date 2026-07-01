75762c4b043c61a0e98c7c1b286bb45b0b9ebf06
GODWIN <godwingod30@gmail.com>
2026-07-01 10:16:10 +0530

    Initial

---
+++ Day1/problem11.java
@@ -0,0 +1,11 @@
+public class problem11 {
+
+    Test() {
+        System.out.println("Constructor Executed");
+    }
+
+    public static void main(String[] args) {
+        new Test();
+    }
+
+}
