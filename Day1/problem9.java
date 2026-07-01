887d1eb4e97627397f91c98b8425eb0588c156aa
GODWIN <godwingod30@gmail.com>
2026-07-01 09:27:18 +0530

    Initial

---
+++ Day1/problem9.java
@@ -0,0 +1,10 @@
+public class problem9 {
+
+    String name = "Harini";
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        System.out.println(s.name);
+    }
+
+}
