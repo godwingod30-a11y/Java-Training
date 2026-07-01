65b06907d86d0f37289f58668877a6f8c791c985
GODWIN <godwingod30@gmail.com>
2026-07-01 11:10:57 +0530

    Initial

---
+++ Day1/problem12.java
@@ -0,0 +1,11 @@
+public class problem12 {
+
+    static {
+        System.out.println("Static Block");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main Method");
+    }
+
+}
