119b78f104ed901ab89a493d77abc4d1618047d3
GODWIN <godwingod30@gmail.com>
2026-07-01 09:30:15 +0530

    Initial

---
+++ Day1/problem10.java
@@ -0,0 +1,11 @@
+public class problem10 {
+
+    void show() {
+        System.out.println("Method Called");
+    }
+
+    public static void main(String[] args) {
+        new MethodDemo().show();
+    }
+
+}
