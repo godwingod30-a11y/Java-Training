bbd9544d4efbe4bb0709ee86be68c73a2ad06d9c
GODWIN <godwingod30@gmail.com>
2026-07-02 15:29:02 +0530

    Initial

---
+++ Day5/addition.java
@@ -0,0 +1,17 @@
+package Day5;
+
+import java.util.Scanner;
+
+public class addition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Sum = " + (a + b));
+    }
+}
