ce0ecfdba7d52614e89b65d9fac489739301497f
GODWIN <godwingod30@gmail.com>
2026-07-02 15:02:26 +0530

    Initial

---
+++ Day4/narrowing1.java
@@ -0,0 +1,10 @@
+package Day4;
+
+public class narrowing1 {
+    public static void main(String[] args) {
+        double d = 45.78;
+        int i = (int) d;
+
+        System.out.println(i);
+    }
+}
