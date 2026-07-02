7b30e473ea1ff0b83d04f7481888e19f0c9f3495
GODWIN <godwingod30@gmail.com>
2026-07-02 14:57:51 +0530

    Initial

---
+++ Day4/widening2.java
@@ -0,0 +1,10 @@
+package Day4;
+
+public class widening2 {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int value = ch;
+
+        System.out.println(value);
+    }
+}
