7a987d74f84a703da9cbfe828a94a22f32ebada0
GODWIN <godwingod30@gmail.com>
2026-07-02 15:09:17 +0530

    Initial

---
+++ Day4/casting11.java
@@ -0,0 +1,10 @@
+package Day4;
+
+public class casting11 {
+    public static void main(String[] args) {
+        int num = 66;
+        char ch = (char) num;
+
+        System.out.println(ch);
+    }
+}
