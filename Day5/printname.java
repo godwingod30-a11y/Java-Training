ec4efa0e65da20cfd200fe384e546a97c92986e4
GODWIN <godwingod30@gmail.com>
2026-07-02 15:18:21 +0530

    Initial

---
+++ Day5/printname.java
@@ -0,0 +1,14 @@
+package Day5;
+
+import java.util.Scanner;
+
+public class printname {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
