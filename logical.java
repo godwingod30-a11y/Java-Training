b839d3b62e53b275c360a86263f78b7d77ce3b29
GODWIN <godwingod30@gmail.com>
2026-07-02 14:07:38 +0530

    Initial

---
+++ logical.java
@@ -0,0 +1,8 @@
+public class logical {
+    public static void main(String[] args) {
+        boolean a = true, b = false;
+        System.out.println(a && b);
+        System.out.println(a || b);
+        System.out.println(!a);
+    }
+}
