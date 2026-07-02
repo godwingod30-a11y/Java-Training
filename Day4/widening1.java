7ef43f80e96effa8152be04c71c05fcf2d54a2ee
GODWIN <godwingod30@gmail.com>
2026-07-02 14:56:17 +0530

    Initial

---
+++ Day4/widening1.java
@@ -0,0 +1,11 @@
+package Day4;
+
+public class widening1 {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = a;
+
+        System.out.println("Integer: " + a);
+        System.out.println("Double: " + b);
+    }
+}
