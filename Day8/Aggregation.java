fe6b77d478315e57ef03d1a62b0a145113bad3b1
GODWIN <godwingod30@gmail.com>
2026-07-14 17:21:08 +0530

    Initial

---
+++ Day8/Aggregation.java
@@ -0,0 +1,38 @@
+package Day8;
+
+class Student {
+
+    String name;
+
+    Student(String name) {
+        this.name = name;
+    }
+
+}
+
+class College {
+
+    Student student;
+
+    College(Student student) {
+        this.student = student;
+    }
+
+    void display() {
+        System.out.println(student.name);
+    }
+
+}
+
+public class Aggregation {
+
+    public static void main(String[] args) {
+
+        Student s = new Student("godwin");
+
+        College c = new College(s);
+
+        c.display();
+
+    }
+}
